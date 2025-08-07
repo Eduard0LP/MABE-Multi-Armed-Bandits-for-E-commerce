0ba0ef69085a9464c602c6ed05f700f9dc9a0a05
Eduardo <eduardolop1601@gmail.com>
2025-08-07 17:09:40 +0200

    __init__.py files added to repository

---
+++ mabe/models/__init__.py
@@ -0,0 +1,7 @@
+from .epsilon_greedy import EpsilonGreedy
+from .lints import LinTS
+from .linucb import LinUCB
+from .ts import ThompsonSampling
+from .ucb import UCB
+
+__all__ = ["EpsilonGreedy", "LinTS", "LinUCB", "ThompsonSampling", "UCB"]
---
+++ mabe/simulator/__init__.py
@@ -0,0 +1,3 @@
+from .simulator import generate_products, generate_user, simulate_interactions
+
+__all__ = ["generate_products", "generate_user", "simulate_interactions"]
